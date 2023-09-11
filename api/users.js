6c546ef5b17ba27649d66d472e4a285a23bd2235
IzabelaMalinowska <114566540+IzabelaMalinowska@users.noreply.github.com>
2023-09-11 17:32:33 +0200

    update

--- api/users.js
+++ api/users.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/users");
-const { auth } = require("../middlewares/auth");
+const { authorizeUser } = require("../middleware/authorization");
+const { uploadMiddleware } = require("../middleware/upload");
 
 router.post("/signup", userController.register);
 router.post("/login", userController.login);
-router.get("/logout", auth, userController.logout);
-router.get("/current", auth, userController.current);
+router.post("verify", userController.resendVerificationMail);
+router.get("/logout", authorizeUser, userController.logout);
+router.get("/current", authorizeUser, userController.current);
+router.get("/verify/:verificationToken", userController.verifyUserByToken);
+router.patch(
+  "/:userId/subscription",
+  authorizeUser,
+  userController.updateSubscription
+);
+router.patch(
+  "/avatars",
+  authorizeUser,
+  uploadMiddleware.single("avatar"),
+  userController.updateAvatar
+);
+router.delete("/", userController.deleteUserByMail);
 
 module.exports = router;
