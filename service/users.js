01b7d62e0cf1fbd1e82f4f53ec1b4993a16a775c
IzabelaMalinowska <114566540+IzabelaMalinowska@users.noreply.github.com>
2023-09-16 20:19:38 +0200

    uppdate

--- service/users.js
+++ service/users.js
@@ -1,78 +1,58 @@
 const User = require("./schemas/user");
 
 const getUser = async (body) => {
-  try {
-    const user = await User.findOne(body);
-    if (!user) {
-      throw new Error("User not found");
-    }
-    return user;
-  } catch (error) {
-    throw error;
+  const user = await User.findOne(body);
+  if (!user) {
+    throw new Error("User not found");
   }
+  return user;
 };
 
 const updateUserSubscription = async (userId, subscription) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      userId,
-      { subscription },
-      { new: true }
-    );
-    if (!updatedUser) {
-      throw new Error("User not found");
-    }
-    return updatedUser;
-  } catch (error) {
-    throw error;
+  const updatedUser = await User.findByIdAndUpdate(
+    userId,
+    { subscription },
+    { new: true }
+  );
+  if (!updatedUser) {
+    throw new Error("User not found");
   }
+  return updatedUser;
 };
 
 const updateUserAvatar = async (userId, avatarURL) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      userId,
-      { avatarURL },
-      { new: true }
-    );
-    if (!updatedUser) {
-      throw new Error("User not found");
-    }
-    return updatedUser;
-  } catch (error) {
-    throw error;
+  const updatedUser = await User.findByIdAndUpdate(
+    userId,
+    { avatarURL },
+    { new: true }
+  );
+  if (!updatedUser) {
+    throw new Error("User not found");
   }
+  return updatedUser;
 };
 
 const deleteUser = async (userMail) => {
-  try {
-    const deletedUser = await User.findOneAndDelete({ email: userMail });
-    if (!deletedUser) {
-      throw new Error("User not found");
-    }
-    return deletedUser;
-  } catch (error) {
-    throw error;
+  const deletedUser = await User.findOneAndDelete({ email: userMail });
+  if (!deletedUser) {
+    throw new Error("User not found");
   }
+  return deletedUser;
 };
 
 const updateUserVerification = async (userId) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      userId,
-      {
-        verificationToken: null,
-        verify: true,
-      },
-      { new: true }
-    );
-    if (!updatedUser) {
-      throw new Error("User not found");
-    }
-    return updatedUser;
-  } catch (error) {
-    throw error;
+  const updatedUser = await User.findByIdAndUpdate(
+    userId,
+    {
+      verificationToken: null,
+      verify: true,
+    },
+    { new: true }
+  );
+  if (!updatedUser) {
+    throw new Error("User not found");
   }
+  return updatedUser;
 };
 
 module.exports = {
